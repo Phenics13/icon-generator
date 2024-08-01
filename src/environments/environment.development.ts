fe2decb0f95a8b9a92e0de1839947ce700d21755
phenics13 <optron920@gmail.com>
2024-08-01 14:30:33 +0300

    changed environments back to process.env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,23 +1,22 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: import.meta.env['NG_APP_FIREBASE_API'] || '',
-    authDomain: import.meta.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || '',
-    projectId: import.meta.env['NG_APP_FIREBASE_PROJECT_ID'] || '',
-    storageBucket: import.meta.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || '',
-    messagingSenderId:
-      import.meta.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || '',
-    appId: import.meta.env['NG_APP_FIREBASE_APP_ID'] || '',
+    apiKey: process.env['NG_APP_FIREBASE_API'] || '',
+    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || '',
+    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || '',
+    storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || '',
+    messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || '',
+    appId: process.env['NG_APP_FIREBASE_APP_ID'] || '',
   },
   cloudinary: {
-    cloudName: import.meta.env['NG_APP_CLOUDINARY_CLOUD_NAME'] || '',
-    uploadPreset: import.meta.env['NG_APP_CLOUDINARY_UPLOAD_PRESET'] || '',
-    apiKey: import.meta.env['NG_APP_CLOUDINARY_API'] || '',
+    cloudName: process.env['NG_APP_CLOUDINARY_CLOUD_NAME'] || '',
+    uploadPreset: process.env['NG_APP_CLOUDINARY_UPLOAD_PRESET'] || '',
+    apiKey: process.env['NG_APP_CLOUDINARY_API'] || '',
   },
   openAI: {
-    key: import.meta.env['NG_APP_OPENAI_API'] || '',
+    key: process.env['NG_APP_OPENAI_API'] || '',
   },
   stripe: {
-    public: import.meta.env['NG_APP_STRIPE_PUBLIC_KEY'] || '',
-    secret: import.meta.env['NG_APP_STRIPE_SECRET_KEY'] || '',
+    public: process.env['NG_APP_STRIPE_PUBLIC_KEY'] || '',
+    secret: process.env['NG_APP_STRIPE_SECRET_KEY'] || '',
   },
 };
